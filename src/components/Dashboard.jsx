1e99dbc83f59c2f30bad5f21f62b0842da7bd0fc
temlena <oridupahelen@gmail.com>
2024-04-21 20:19:07 +0100

    styled dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,42 +1,43 @@
 import dashboardData from "./dashboardData.js";
 import message from "../assets/message-icon.svg";
+import notification from "../assets/Bell_Notification.svg";
 import checkbox from "../assets/Interface.svg";
 export default function Dashboard() {
   return (
     <div className="bg-text px-16 py-8">
-      <div className="p-4 flex justify-between">
+      <div className="p-4 flex justify-between items-center">
         <div>
-          <h1>Hello Evangeline! Welcome Back</h1>
-          <p>
-            Your next payment of <strong>10000</strong> is scheduled for{" "}
-            <span className="text-red-800">
-              {" "}
-              <b>May 29 2024</b>
-            </span>
+          <h1 className="font-semibold text-2xl">
+            Hello Evangeline! Welcome Back
+          </h1>
+          <p className="font-normal text-xl">
+            Your next payment of <span className="font-bold">10000</span> is
+            scheduled for{" "}
+            <span className="font-bold text-xl text-error"> May 29 2024</span>
           </p>
         </div>
 
-        <div>
-          <img src={message} alt="message-icon" />
-          <img src="" alt="bell-icon" />
+        <div className="flex gap-2 justify-center items-center">
+          <img className="w-6" src={message} alt="message-icon" />
+          <img className="w-6" src={notification} alt="bell-icon" />
         </div>
       </div>
 
       <div className="flex justify-between items-center">
-        <div className="text-text p-4 rounded bg-primary w-40 h-20">
-          <p className="font-bold">All loan amount</p>
-          <p>#1,500,000</p>
+        <div className="text-text p-4 rounded bg-primary w-44 ">
+          <p className="font-normal text-lg">All loan amount</p>
+          <p className="font-normal text-2xl">#1,500,000</p>
         </div>
-        <div className="text-primary p-4 rounded bg-service-background w-40">
-          <p className="font-bold">Amount paid</p>
-          <p>#1,500,000</p>
+        <div className="text-primary p-4 rounded bg-dashboard w-44">
+          <p className="font-normal text-lg">Amount paid</p>
+          <p className="font-normal text-2xl">#1,500,000</p>
         </div>
-        <div className="text-primary p-4 rounded bg-service-background w=40">
-          <p className="font-bold">Total loan</p>
-          <p>2</p>
+        <div className="text-primary p-4 rounded bg-dashboard w-44">
+          <p className="font-normal text-lg">Total loan</p>
+          <p className="font-normal text-2xl">2</p>
         </div>
       </div>
-      <table className="w-full">
+      <table className="w-full mt-2">
         <tbody>
           {dashboardData.map((val, key) => {
             return (
@@ -50,7 +51,15 @@ export default function Dashboard() {
                 <td className="p-4">{val.Info}</td>
                 <td className="p-4">{val.Time}</td>
                 <td className="p-4">{val.Amount}</td>
-                <td className="p-4">{val.Status}</td>
+                <td
+                  className={`${
+                    val.Status === "Successful"
+                      ? "text-popular-colour"
+                      : "text-error"
+                  } text-xl font-medium`}
+                >
+                  {val.Status}
+                </td>
               </tr>
             );
           })}
