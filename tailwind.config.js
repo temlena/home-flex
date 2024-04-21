c2b080ee5ca1293f1ef407853c1965df367a9521
temlena <oridupahelen@gmail.com>
2024-04-21 20:19:07 +0100

    styled dashboard component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/*{js,jsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", "./src/*{js,jsx}"],
   theme: {
     extend: {
-      backgroundImage:{
-        'hero-bg': "url('../src/assets/image 6.png')",
+      backgroundImage: {
+        "hero-bg": "url('../src/assets/image 6.png')",
       },
-      fontFamily:{
+      fontFamily: {
         brico: "'Bricolage Grotesque', serif",
       },
-      colors:{
-        'primary':'#17004A',
-        'text': '#FFFFFF',
-        'service': '#564A72',
-        'service-background': '#F3FBF8',
-        'service-card':'#FFF',
-        'popular-colour':'#003A26',
-        'btn-primary':'#003588',
-        'question-background':'#FFF3E6',
-        'question-primary':'#FF8600',
-        'title-colour':'#201F1F'
-
-      }
+      colors: {
+        primary: "#17004A",
+        text: "#FFFFFF",
+        service: "#564A72",
+        "service-background": "#F3FBF8",
+        "service-card": "#FFF",
+        "popular-colour": "#003A26",
+        "btn-primary": "#003588",
+        "question-background": "#FFF3E6",
+        "question-primary": "#FF8600",
+        "title-colour": "#201F1F",
+        "error": "#EF1515",
+        "dashboard": "#E6ECF7",
+      },
     },
   },
   plugins: [],
-}
-
+};
